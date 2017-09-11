37fe5564e623e88a99580a1dbf836ca8ed451c36
Shimi66 <31352848+E-Voss66@users.noreply.github.com>
2017-09-11 11:56:27 -0700

    Lab 4 Day One

--- BeeperBot.java
+++ BeeperBot.java
@@ -3,7 +3,7 @@ import kareltherobot.*;
 
 
 /**
- * @author :  D. Appel
+ * @author :  E. Voss
  */
 public class BeeperBot extends Robot
 {
@@ -12,7 +12,48 @@ public class BeeperBot extends Robot
     }
     
     public void runRace() {
-        
+        while(frontIsClear()) {
+            collectBeeperSteeple();
+        }
+    }
+    
+    public void findBeepers() {
+        faceEast();
+        while(!nextToABeeper() && frontIsClear()) {
+            move();
+        }
+    }
+    
+    public void faceEast() {
+        while(!facingEast()) {
+            turnLeft();
+        }
+    }
+    
+    public void faceNorth() {
+        while(!facingNorth()) {
+            turnLeft();
+        }
+    }
+    
+    public void faceSouth() {
+        while(!facingSouth()) {
+            turnLeft();
+        }
+    }
+    
+    public void collectBeeperSteeple() {
+        findBeepers();
+        faceNorth();
+        while(nextToABeeper()) {
+            move();
+        }
+        faceSouth();
+        while(frontIsClear()) {
+            move();
+            pickBeeper();
+        }
+        faceEast();
     }
    
 }
